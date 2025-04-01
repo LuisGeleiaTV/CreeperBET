d30dd3ee39adc0e003ab9368993d5be2f8933d1c
LuisGeleiaTV <119079587+LuisGeleiaTV@users.noreply.github.com>
2025-03-31 23:57:16 -0300

    Add files via upload

---
+++ rename_blocks.bat
@@ -0,0 +1,21 @@
+@echo off
+cd assets\blocks
+ren "Dirt_%28texture%29_JE2_BE2.webp" "dirt.webp"
+ren "Stone_%28texture%29_JE5_BE3.webp" "stone.webp"
+ren "Cobblestone_%28texture%29_JE5_BE3.webp" "cobblestone.webp"
+ren "Birch_Planks_%28texture%29_JE3_BE2.webp" "birch_planks.webp"
+ren "Stone_Bricks_%28texture%29_JE3_BE2.webp" "stone_bricks.webp"
+ren "Block_of_Gold_%28texture%29_JE5_BE3.webp" "gold_block.webp"
+ren "Diamond_Ore_%28texture%29_JE5_BE5.webp" "diamond_ore.webp"
+ren "Glowstone_%28texture%29_JE4_BE3.webp" "glowstone.webp"
+ren "Sponge_%28texture%29_JE3_BE3.webp" "sponge.webp"
+ren "Oak_Log_%28texture%29_JE4_BE2.webp" "oak_log.webp"
+ren "Block_of_Quartz_%28top_texture%29_JE3_BE3.webp" "quartz_block.webp"
+ren "Deepslate_Emerald_Ore_%28texture%29_JE1_BE1.webp" "emerald_ore.webp"
+ren "Iron_Ore_%28texture%29_JE6_BE4.webp" "iron_ore.webp"
+ren "End_Stone_%28texture%29_JE3_BE2.webp" "end_stone.webp"
+ren "Polished_Andesite_%28texture%29_JE2_BE2.webp" "polished_andesite.webp"
+ren "Smooth_Stone_%28texture%29_JE2_BE2.webp" "smooth_stone.webp"
+ren "Ice_%28texture%29_JE2_BE6.webp" "ice.webp"
+echo Renomeação concluída!
+pause 
